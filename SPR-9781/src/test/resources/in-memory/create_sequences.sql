deabfb8a0e3483588fb0befc9898c53c87f6992c
Philipp Hügelmeyer <p.huegelmeyer@gmail.com>
2012-09-10 11:02:47 +0200

    Initialer Checkin

---
+++ SPR-9781/src/test/resources/in-memory/create_sequences.sql
@@ -0,0 +1,39 @@
+-- Anlegen der Sequences und Kommentare
+-- Die in < > gesetzten Werte muessen angegeben werden
+--
+-- Erstellt: 20.12.2011 durch: Marchand
+
+------------------------------------------------------------
+
+create sequence betrieb_seq start with 1;
+create sequence betriebsdaten_seq start with 1;
+create sequence konstante_seq start with 1;
+create sequence einkommen_seq start with 1;
+create sequence berggebiet_seq start with 1;
+create sequence merkmal_seq start with 1;
+create sequence standardoutput_seq start with 1;
+create sequence rules_seq start with 1;
+create sequence statement_seq start with 1;
+create sequence status_seq start with 1;
+create sequence berechnungsversion_seq start with 1;
+create sequence versionbetrieb_seq start with 1;
+create sequence komplexevariable_seq start with 1;
+create sequence grundgesamtheit_seq start with 1;
+create sequence grundgesamtheitsdaten_seq start with 1;
+create sequence auswertungszeitraum_seq start with 1;
+create sequence auswahlversionbetrieb_seq start with 1;
+create sequence gewichtungsfaktor_seq start with 1;
+create sequence land_seq start with 1;
+create sequence bezirk_seq start with 1;
+create sequence berichtslevel_seq start with 1;
+create sequence betriebmapping_seq start with 1;
+create sequence erhebungsbereich_seq start with 1;
+create sequence bereich_seq start with 1;
+create sequence abschnitt_seq start with 1;
+create sequence zeile_seq start with 1;
+create sequence spalte_seq start with 1;
+create sequence variable_seq start with 1;
+create sequence rolle_seq start with 1;
+create sequence zugang_seq start with 1;
+create sequence jobprotokoll_seq start with 1;
+create sequence hibernate_sequence start with 1;
